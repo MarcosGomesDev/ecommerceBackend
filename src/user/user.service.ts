f9cf3e2d932637a061e1bb8564e7e8dc1cb2dbce
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-17 00:58:26 -0300

    feat: add typeOrm and UserEntity

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const passwordHashed = await hash(createUserDto.password, 10);
+
+    return this.userRepository.save({
+      ...createUserDto,
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+}
