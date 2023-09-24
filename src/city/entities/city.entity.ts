385a26b54b9f1b761977191e4802de3b6f4f6b88
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:07:39 -0300

    feat: create city entity

---
+++ src/city/entities/city.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'city' })
+export class CityEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'state_id', nullable: false })
+  stateId: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
