67e319c36a9c0d1ff5ad927cda957f862698e27f
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:07:54 -0300

    feat: create city module

---
+++ src/city/city.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
+
+@Module({
+  imports: [
+    CacheModule.register({ ttl: 900000000 }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+})
+export class CityModule {}
