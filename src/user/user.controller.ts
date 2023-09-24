63b0c2ab67d94a18a7833df3f698df5bef6567ea
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:46:42 -0300

    feat: add Pipe Validation and insert Return User DTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,30 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 
   @Get()
-  async getAllUsers(): Promise<UserEntity[]> {
-    return this.userService.getAllUsers();
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
