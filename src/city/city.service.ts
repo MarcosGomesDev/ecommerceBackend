c9134b329e9232a5a46f12bb8e9902f562b61bea
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:08:11 -0300

    feat: create city service

---
+++ src/city/city.service.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+@Injectable()
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  async getAllCitiesByState(stateId: number): Promise<CityEntity[]> {
+    const citiesCache: CityEntity[] = await this.cacheManager.get(`${stateId}`);
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
+      where: {
+        stateId,
+      },
+    });
+
+    await this.cacheManager.set(`${stateId}`, cities);
+
+    return cities;
+  }
+}
