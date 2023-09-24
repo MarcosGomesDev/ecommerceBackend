c7cde79d29d393a887557856ebc00bba1122d8a2
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:06:18 -0300

    feat: create state entity

---
+++ src/state/entities/state.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'state' })
+export class StateEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
