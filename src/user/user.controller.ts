aea940be832aaeb06790b9debb868f10cf5eb83e
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-24 00:05:43 -0300

    feat: create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
-import { UserEntity } from './interfaces/user.entity';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
