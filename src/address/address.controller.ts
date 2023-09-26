91d9e7901fb2346e421e7700827486c9bcc26bf5
Marcos Gomes <marcosgomesdev@gmail.com>
2023-09-25 22:24:53 -0300

    feat: insert authorization routes jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -9,7 +9,10 @@ import {
 import { CreateAddressDto } from './dto/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
