194fe5dc1fb4ab19b81309508dafdb996b4904b2
Marcos Gomes <marcosgomesdev@gmail.com>
2023-10-22 19:03:28 -0300

    feat: insert function create product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { Roles } from 'src/decorators/roles.decorator';
-import { UserType } from 'src/user/enum/user-type.enum';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
 import { ReturnProduct } from './dto/returnProduct.dto';
+import { CreateProduct } from './dto/create-product.dto';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('product')
@@ -22,5 +25,14 @@ export class ProductController {
     return (await this.productService.findAll()).map(
       (product) => new ReturnProduct(product),
     );
+  }
+
+  @Roles(UserType.Admin)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createProduct(
+    @Body() createProduct: CreateProduct,
+  ): Promise<ReturnProduct> {
+    return await this.productService.createProduct(createProduct);
   }
 }
